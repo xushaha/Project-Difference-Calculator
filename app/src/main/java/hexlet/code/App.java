031f1d6c493694817fec2d1a5ba4e0e92d4b4bea
xushaha <xushaha@ya.ru>
2023-01-11 14:46:17 +0300

    sixth step_1

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
-import picocli.CommandLine.Option;
+//import picocli.CommandLine.Option;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff",
@@ -16,12 +16,12 @@ public class App implements Callable<Integer> {
 /*        @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
         private String format = "output format [default: stylish]";*/
 
-        @Parameters(paramLabel = "filepath1",
+    @Parameters(paramLabel = "filepath1",
                 index = "0",
                 description = "path to first file")
         private String filePath1;
 
-        @Parameters(paramLabel = "filepath2",
+    @Parameters(paramLabel = "filepath2",
                 index = "1",
                 description = "path to second file")
         private String filePath2;
@@ -31,24 +31,23 @@ public class App implements Callable<Integer> {
                 description = "name of format")
         private String formatName;*/
 
-        int ERROR_EXIT_CODE;
-        int SUCCESS_EXIT_CODE;
+    private static final int SUCCESS_EXIT_CODE = 0;
+    private static final int ERROR_EXIT_CODE = 1;
 
-        @Override
-        public Integer call() throws Exception {
-                try {
-                        String formattedDiff = Differ.generate(filePath1, filePath2);
-                        System.out.println(formattedDiff);
-                } catch (Exception e) {
-                        System.err.println(e.getMessage());
-                        return ERROR_EXIT_CODE;
-                }
-
-                return SUCCESS_EXIT_CODE;
+    @Override
+    public Integer call() throws Exception {
+        try {
+            String formattedDiff = Differ.generate(filePath1, filePath2);
+            System.out.println(formattedDiff);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return ERROR_EXIT_CODE;
         }
+        return SUCCESS_EXIT_CODE;
+    }
 
-        public static void main(String[] args) {
-                int exitCode = new CommandLine(new App()).execute(args);
-                System.exit(exitCode);
-        }
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,13 +6,13 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.LinkedHashMap;
+//import java.util.LinkedHashMap;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 public class Differ {
-        static String generate(String filePath1, String filePath2) throws Exception {
+    static String generate(String filePath1, String filePath2) throws Exception {
 
 /*              Метод generate() должен содержать процесс:
                 - чтение файлов в строковые переменные
@@ -21,68 +21,67 @@ public class Differ {
                 - построение разницы между данными
                 - форматирование разницы в необходимом формате (введён пользователем или дефолтный)*/
 
-                // Формируем путь абсолютный путь, если filePath будет содержать относительный путь,
-                // то мы всегда будет работать с абсолютным
-                Path path1 = Path.of(filePath1).toAbsolutePath().normalize();
-                Path path2 = Path.of(filePath2).toAbsolutePath().normalize();
+        // Формируем путь абсолютный путь, если filePath будет содержать относительный путь,
+        // то мы всегда будет работать с абсолютным
+        Path path1 = Path.of(filePath1).toAbsolutePath().normalize();
+        Path path2 = Path.of(filePath2).toAbsolutePath().normalize();
 
                 // Проверяем существование файла
-                if (!Files.exists(path1)) {
-                        throw new Exception("File '" + path1 + "' does not exist");
-                }
-                if (!Files.exists(path2)) {
-                        throw new Exception("File '" + path2 + "' does not exist");
-                }
-
-                // Читаем файлы
-                String content1 = Files.readString(path1);
-                String content2 = Files.readString(path2);
-
-                // Строку в мапу
-                Map<String, Object> map1 = stringToMap(content1);
-                Map<String, Object> map2 = stringToMap(content2);
-
-                // результирующая мапа
-                Map<String, Object> result = new HashMap<>();
-
-                Set<String> keys = new TreeSet<>(map1.keySet());
-                keys.addAll(map2.keySet());
-
-                for (String key: keys) {
-                        String keyRemoved = "- " + key;
-                        String keyAdded = "+ " + key;
-                        if (!map1.containsKey(key)) {
-                               // ключ добавили, надо с плюсом
-                                result.put(keyAdded, map2.get(key));
-                        } else if (!map2.containsKey(key)) {
-                                // ключ удалили, надо с минусом
-                                result.put(keyRemoved, map1.get(key));
-                        } else if (map1.containsKey(key) && map2.containsKey(key)) {
-                                // проверяем равны ли значения
-                                // если равны, то выводим без добавок
-                                if (map1.get(key).equals(map2.get(key))) {
-                                    result.put(key, map1.get(key));
-                                // если разные, то первый с минусом второй с плюсом
-                                } else {
-                                        result.put(keyRemoved, map1.get(key));
-                                        result.put(keyAdded, map2.get(key));
-                                }
-                        }
-                }
-
-                return mapToString(result);
+        if (!Files.exists(path1)) {
+            throw new Exception("File '" + path1 + "' does not exist");
+        }
+        if (!Files.exists(path2)) {
+            throw new Exception("File '" + path2 + "' does not exist");
         }
 
+        // Читаем файлы
+        String content1 = Files.readString(path1);
+        String content2 = Files.readString(path2);
 
+        // Строку в мапу
+        Map<String, Object> map1 = stringToMap(content1);
+        Map<String, Object> map2 = stringToMap(content2);
 
-        public static Map stringToMap(String content) throws Exception {
-                ObjectMapper mapper = new ObjectMapper();
-                return mapper.readValue(content, Map.class);
+        // результирующая мапа
+        Map<String, Object> result = new HashMap<>();
+
+        Set<String> keys = new TreeSet<>(map1.keySet());
+        keys.addAll(map2.keySet());
+
+        for (String key: keys) {
+            String keyRemoved = "- " + key;
+            String keyAdded = "+ " + key;
+            if (!map1.containsKey(key)) {
+                // ключ добавили, надо с плюсом
+                result.put(keyAdded, map2.get(key));
+            } else if (!map2.containsKey(key)) {
+                // ключ удалили, надо с минусом
+                result.put(keyRemoved, map1.get(key));
+            } else if (map1.containsKey(key) && map2.containsKey(key)) {
+                // проверяем равны ли значения
+                // если равны, то выводим без добавок
+                if (map1.get(key).equals(map2.get(key))) {
+                    result.put(key, map1.get(key));
+                // если разные, то первый с минусом второй с плюсом
+                } else {
+                    result.put(keyRemoved, map1.get(key));
+                    result.put(keyAdded, map2.get(key));
+                }
+            }
         }
 
-        public static String mapToString(Map<String, Object> map) throws Exception {
-                ObjectMapper mapper = new ObjectMapper();
-                return mapper.writeValueAsString(map);
-        }
+        return mapToString(result);
+    }
+
+
+    public static Map stringToMap(String content) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(content, Map.class);
+    }
+
+    public static String mapToString(Map<String, Object> map) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(map);
+    }
 }
 
